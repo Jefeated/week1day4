869e5e03f40603c61ff80eff913871831e398aca
Jeff <jeffng@live.ca>
2017-03-10 08:29:01 +0000

    sort completed

--- sort.js
+++ sort.js
@@ -1,1 +1,22 @@
-sort.js
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a, b) {
+
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  if (a.name === b.name) {
+  students.sort(function (a, b) {
+  return a.age - b.age;
+  }
+)};
+});
+    console.log(students);
